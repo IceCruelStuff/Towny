8df0df0e0304eea40061721a0ec0ac2416784abd
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-11-08 15:51:24 -0800

    Update CommandManager.php

--- src/Towny/command/CommandManager.php
+++ src/Towny/command/CommandManager.php
@@ -1,20 +1,26 @@
 <?php
+
 declare(strict_types=1);
+
 namespace Towny\command;
 
 use pocketmine\Server;
 use Towny\command\subcommand\CreateSubCommand;
 use Towny\TownyLoader;
 
-class CommandManager{
+class CommandManager
+{
 
-	public static function registerAll(TownyLoader $plugin) : void{
-		$server = Server::getInstance();
-		$server->getCommandMap()->register("towny", new TownyCommand(TownyLoader::getInstance()->getLanguage()->translateString("towny.command.name"), TownyLoader::getInstance()->getLanguage()->translateString("towny.command.description")));
+    public static function registerAll(TownyLoader $plugin) : void
+    {
+        $server = Server::getInstance();
+        $server->getCommandMap()->register("towny", new TownyCommand(TownyLoader::getInstance()->getLanguage()->translateString("towny.command.name"),
+                                                                     TownyLoader::getInstance()->getLanguage()->translateString("towny.command.description")));
 
-		$command = $server->getCommandMap()->getCommand(TownyLoader::getInstance()->getLanguage()->translateString("towny.command.name"));
-		if($command instanceof TownyCommand){
-			$command->addSubCommand(new CreateSubCommand($plugin));
-		}
-	}
+        $command = $server->getCommandMap()->getCommand(TownyLoader::getInstance()->getLanguage()->translateString("towny.command.name"));
+        if ($command instanceof TownyCommand) {
+            $command->addSubCommand(new CreateSubCommand($plugin));
+        }
+    }
+
 }
