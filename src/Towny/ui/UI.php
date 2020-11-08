8160943ea431c089891749dd4d4b387ba69f4f18
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-11-08 15:38:40 -0800

    Update UI.php

--- src/Towny/ui/UI.php
+++ src/Towny/ui/UI.php
@@ -1,37 +1,44 @@
 <?php
+
 declare(strict_types=1);
+
 namespace Towny\ui;
 
 use pocketmine\player\Player;
 use Towny\TownyLoader;
 
-abstract class UI{
+abstract class UI
+{
 
-	private static $formIds = [];
+    private static $formIds = [];
 
-	private static $plugin;
+    private static $plugin;
 
-	public static $uiMessage = "towny.ui.";
+    public static $uiMessage = "towny.ui.";
 
-	public static $message = "towny.message.";
+    public static $message = "towny.message.";
 
-	public static function init(){
-		self::$plugin = TownyLoader::getInstance();
+    public static function init()
+    {
+        self::$plugin = TownyLoader::getInstance();
 
-		self::$formIds[CreateUI::getFormId()] = new CreateUI();
-	}
+        self::$formIds[CreateUI::getFormId()] = new CreateUI();
+    }
 
-	final public static function getPlugin() : TownyLoader{
-		return self::$plugin ?? self::$plugin = TownyLoader::getInstance();
-	}
+    final public static function getPlugin() : TownyLoader
+    {
+        return self::$plugin ?? self::$plugin = TownyLoader::getInstance();
+    }
 
-	final public static function getFormById(int $id) : ?UI{
-		return self::$formIds[$id] ?? null;
-	}
+    final public static function getFormById(int $id) : ?UI
+    {
+        return self::$formIds[$id] ?? null;
+    }
 
-	abstract public static function getFormId() : int;
+    abstract public static function getFormId() : int;
 
-	abstract public static function sendToPlayer(Player $player) : void;
+    abstract public static function sendToPlayer(Player $player) : void;
 
-	abstract public function handleResponse(Player $player, $data) : void;
+    abstract public function handleResponse(Player $player, $data) : void;
+
 }
