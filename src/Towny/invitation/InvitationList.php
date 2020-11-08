c766a13ea20f696bd4699629a971ce4c6aac6ed3
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-11-08 15:43:35 -0800

    Update InvitationList.php

--- src/Towny/invitation/InvitationList.php
+++ src/Towny/invitation/InvitationList.php
@@ -1,57 +1,66 @@
 <?php
+
 declare(strict_types=1);
+
 namespace Towny\invitation;
 
 use pocketmine\nbt\tag\CompoundTag;
 
-class InvitationList{
+class InvitationList
+{
 
-	/** @var Invitation[] */
-	private $invitations = [];
+    /** @var Invitation[] */
+    private $invitations = [];
 
-	public function __construct(array $invitations){
-		foreach($invitations as $invitation){
-			if($invitation !== null){
-				$this->add($invitation);
-			}
-		}
-	}
+    public function __construct(array $invitations)
+    {
+        foreach ($invitations as $invitation) {
+            if ($invitation !== null) {
+                $this->add($invitation);
+            }
+        }
+    }
 
-	public function add(Invitation $invitation){
-		$this->invitations[$invitation->getHash()] = $invitation;
-	}
+    public function add(Invitation $invitation)
+    {
+        $this->invitations[$invitation->getHash()] = $invitation;
+    }
 
-	public function remove(Invitation $invitation){
-		unset($this->invitations[$invitation->getHash()]);
-	}
+    public function remove(Invitation $invitation)
+    {
+        unset($this->invitations[$invitation->getHash()]);
+    }
 
-	/**
-	 * @return Invitation[]
-	 */
-	public function all() : array{
-		return array_values($this->invitations);
-	}
+    /**
+     * @return Invitation[]
+     */
+    public function all() : array
+    {
+        return array_values($this->invitations);
+    }
 
-	public function nbtSerialize() : CompoundTag{
-		if(!empty($this->invitations)){
-			$tag = CompoundTag::create();
-			foreach($this->invitations as $invitation){
-				$tag->setTag($invitation->getPlayer(), $invitation->nbtSerialize());
-			}
+    public function nbtSerialize() : CompoundTag
+    {
+        if (!empty($this->invitations)) {
+            $tag = CompoundTag::create();
+            foreach ($this->invitations as $invitation) {
+                $tag->setTag($invitation->getPlayer(), $invitation->nbtSerialize());
+            }
 
-			return CompoundTag::create()
-					->setTag("Invitations", $tag);
-		}
-		return CompoundTag::create();
-	}
+            return CompoundTag::create()->setTag("Invitations", $tag);
+        }
+        return CompoundTag::create();
+    }
 
-	public static function nbtDeserialize(CompoundTag $nbt) : InvitationList{
-		$arr = [];
-		foreach($nbt->getValue() as $name => $tag){
-			if($tag instanceof CompoundTag){
-				$arr[] = Invitation::nbtDeserialize($tag);
-			}
-		}
-		return new InvitationList($arr);
-	}
+    public static function nbtDeserialize(CompoundTag $nbt) : InvitationList
+    {
+        $arr = [];
+        foreach ($nbt->getValue() as $name => $tag) {
+            if ($tag instanceof CompoundTag) {
+                $arr[] = Invitation::nbtDeserialize($tag);
+            }
+        }
+        return new InvitationList($arr);
+    }
+
 }
