422a0a4756c665b7b9f6259392e5429367f22c0d
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-11-08 15:35:25 -0800

    Update TownyLoader.php

--- src/Towny/TownyLoader.php
+++ src/Towny/TownyLoader.php
@@ -1,5 +1,7 @@
 <?php
+
 declare(strict_types=1);
+
 namespace Towny;
 
 use pocketmine\nbt\LittleEndianNbtSerializer;
@@ -10,71 +12,81 @@ use Towny\lang\PluginLang;
 use Towny\task\TownyCheckTask;
 use Towny\ui\UI;
 
-class TownyLoader extends PluginBase{
+class TownyLoader extends PluginBase
+{
 
-	public static $prefix;
+    public static $prefix;
 
-	/** @var PluginLang */
-	protected $lang;
+    /** @var PluginLang */
+    protected $lang;
 
-	private static $instance = null;
+    private static $instance = null;
 
-	/** @var TownyFactory */
-	protected $townyFactory;
+    /** @var TownyFactory */
+    protected $townyFactory;
 
-	/** @var EventListener */
-	private $listener;
+    /** @var EventListener */
+    private $listener;
 
-	public function onLoad(){
-		self::$instance = $this;
-	}
+    public function onLoad()
+    {
+        self::$instance = $this;
+    }
 
-	public static function getInstance() : TownyLoader{
-		return self::$instance;
-	}
+    public static function getInstance() : TownyLoader
+    {
+        return self::$instance;
+    }
 
-	public function onEnable(){
-		$this->lang = new PluginLang($this);
+    public function onEnable()
+    {
+        $this->lang = new PluginLang($this);
 
-		self::$prefix = $this->lang->translateString("plugin.prefix");
-		$this->getLogger()->info($this->lang->translateString("plugin.enabled"));
+        self::$prefix = $this->lang->translateString("plugin.prefix");
+        $this->getLogger()->info($this->lang->translateString("plugin.enabled"));
 
-		$this->townyFactory = new TownyFactory($this);
-		$this->listener = new EventListener($this);
+        $this->townyFactory = new TownyFactory($this);
+        $this->listener = new EventListener($this);
 
-		$this->getScheduler()->scheduleRepeatingTask(new TownyCheckTask(), 20);
+        $this->getScheduler()->scheduleRepeatingTask(new TownyCheckTask(), 20);
 
-		UI::init();
+        UI::init();
 
-		CommandManager::registerAll($this);
-	}
+        CommandManager::registerAll($this);
+    }
 
-	public function getEventListener() : EventListener{
-		return $this->listener;
-	}
+    public function getEventListener() : EventListener
+    {
+        return $this->listener;
+    }
 
-	public function getTownyFactory() : TownyFactory{
-		return $this->townyFactory;
-	}
+    public function getTownyFactory() : TownyFactory
+    {
+        return $this->townyFactory;
+    }
 
-	public function onDisable(){
-		$this->getLogger()->info($this->lang->translateString("plugin.disabled"));
-		foreach($this->townyFactory->getAllTownies() as $towny){
-			$nbt = $towny->nbtSerialize();
-			file_put_contents($this->getDataFolder() . "towns/" . $towny->getName() . ".dat", (new LittleEndianNbtSerializer())->writeCompressed(new TreeRoot($nbt)));
-		}
-	}
+    public function onDisable()
+    {
+        $this->getLogger()->info($this->lang->translateString("plugin.disabled"));
+        foreach ($this->townyFactory->getAllTownies() as $towny) {
+            $nbt = $towny->nbtSerialize();
+            file_put_contents($this->getDataFolder() . "towns/" . $towny->getName() . ".dat", (new LittleEndianNbtSerializer())->writeCompressed(new TreeRoot($nbt)));
+        }
+    }
 
-	/**
-	 * Need to PluginLang
-	 * @see PluginLang::__construct()
-	 * @return string
-	 */
-	public function getFile() : string{
-		return parent::getFile();
-	}
+    /**
+     * Need to PluginLang
+     * @see PluginLang::__construct()
+     * @return string
+     */
+    public function getFile() : string
+    {
+        return parent::getFile();
+    }
 
-	public function getLanguage() : PluginLang{
-		return $this->lang;
-	}
+    public function getLanguage() : PluginLang
+    {
+        return $this->lang;
+    }
+
 }
