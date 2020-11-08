3d618064ebb4cac7ffdcceca5aea9d4611e369d7
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-11-08 15:53:53 -0800

    Update SubCommand.php

--- src/Towny/command/subcommand/SubCommand.php
+++ src/Towny/command/subcommand/SubCommand.php
@@ -5,38 +5,45 @@ namespace Towny\command\subcommand;
 use pocketmine\command\CommandSender;
 use Towny\TownyLoader;
 
-abstract class SubCommand{
+abstract class SubCommand
+{
 
-	protected $name;
+    protected $name;
 
-	protected $isOp;
+    protected $isOp;
 
-	protected $plugin;
+    protected $plugin;
 
-	public function __construct(TownyLoader $plugin, string $name, bool $isOp = false){
-		$this->name = $name;
-		$this->isOp = $isOp;
-		$this->plugin = $plugin;
-	}
+    public function __construct(TownyLoader $plugin, string $name, bool $isOp = false)
+    {
+        $this->name = $name;
+        $this->isOp = $isOp;
+        $this->plugin = $plugin;
+    }
 
-	public function getPlugin() : TownyLoader{
-		return $this->plugin ?? $this->plugin = TownyLoader::getInstance();
-	}
+    public function getPlugin() : TownyLoader
+    {
+        return $this->plugin ?? $this->plugin = TownyLoader::getInstance();
+    }
 
-	public function hasPermission(CommandSender $sender) : bool{
-		if($this->isOp){
-			return $sender->isOp();
-		}
-		return true;
-	}
+    public function hasPermission(CommandSender $sender) : bool
+    {
+        if ($this->isOp) {
+            return $sender->isOp();
+        }
+        return true;
+    }
 
-	public function getName() : string{
-		return $this->name;
-	}
+    public function getName() : string
+    {
+        return $this->name;
+    }
 
-	public function equals(string $name) : bool{
-		return $this->getName() === $name;
-	}
+    public function equals(string $name) : bool
+    {
+        return $this->getName() === $name;
+    }
 
-	abstract public function handle(CommandSender $sender, array $parameters) : void;
+    abstract public function handle(CommandSender $sender, array $parameters) : void;
+
 }
