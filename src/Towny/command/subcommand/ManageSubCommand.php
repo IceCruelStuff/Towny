6c6dd90916166ea513d3c50f3913f59f67ce3bbf
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-11-08 15:53:17 -0800

    Update ManageSubCommand.php

--- src/Towny/command/subcommand/ManageSubCommand.php
+++ src/Towny/command/subcommand/ManageSubCommand.php
@@ -1,17 +1,23 @@
 <?php
+
 declare(strict_types=1);
+
 namespace Towny\command\subcommand;
 
 use pocketmine\command\CommandSender;
 use Towny\TownyLoader;
 
-class ManageSubCommand extends SubCommand{
+class ManageSubCommand extends SubCommand
+{
 
-	public function __construct(TownyLoader $plugin) {
-		parent::__construct($plugin, $plugin->getLanguage()->translateString("towny.subcommand.manage"), false);
-	}
+    public function __construct(TownyLoader $plugin)
+    {
+        parent::__construct($plugin, $plugin->getLanguage()->translateString("towny.subcommand.manage"), false);
+    }
 
-	public function handle(CommandSender $sender, array $parameters) : void{
+    public function handle(CommandSender $sender, array $parameters) : void
+    {
 
-	}
+    }
+
 }
